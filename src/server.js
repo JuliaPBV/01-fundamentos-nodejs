52124467e9322f71f8160f79f85093fb5d85bd86
Julia Pereira <vital.pereira.julia@gmail.com>
2024-12-06 21:54:48 -0300

    Adicionei o UUID

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import http from "node:http";
+import { randomUUID } from "node:crypto";
 import { json } from "./midllewares/json.js";
 import { Database } from "./midllewares/database.js";
 
@@ -19,7 +20,7 @@ const server = http.createServer(async (req, res) => {
     const { name, email } = req.body;
 
     const user = {
-      id: 1,
+      id: randomUUID(),
       name,
       email,
     };
